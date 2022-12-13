b0ee7f5eeae426032931e449438314126d615d00
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-14 10:08:41 +1100

    add state slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,24 @@
-import {configureStore} from "@reduxjs/toolkit";
-import id from "faker/lib/locales/id_ID";
+import {configureStore, createSlice} from "@reduxjs/toolkit";
 
 const initialState = {counter: 0, showCounter: true};
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggelCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
