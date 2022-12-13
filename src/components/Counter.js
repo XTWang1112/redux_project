5eccc42595581c200b67c369fc63b112d5eed52c
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-14 09:49:35 +1100

    working multiple state data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,12 @@
 import classes from "./Counter.module.css";
 import {Component} from "react";
 import {useSelector, useDispatch, connect} from "react-redux";
+import {getByDisplayValue} from "@testing-library/react";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
     dispatch({type: "increment"});
   };
@@ -16,16 +17,20 @@ const Counter = () => {
     dispatch({type: "decrement"});
   };
 
+  const toggleHandler = () => {
+    dispatch({type: "toggle"});
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleHandler}>Toggle Counter</button>
     </main>
   );
 };
